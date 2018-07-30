ed7c9cac29641766dc0e8eb931552703fbd39729
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-07-29 21:12:30 -0300

    apresentação dos dados em uma tabela

--- app/route/produtos.js
+++ app/route/produtos.js
@@ -1,22 +1,31 @@
 /*
  * Arquivo de configuração das rotas do projeto
  */
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ispj4lqzx22',
+    database: 'alura'
+});
+
 module.exports = function(app){
     app.get('/produtos', function(req, res){
-        var mysql = require('mysql');
-        var conn = mysql.createConnection({
-            host: 'leonardo',
-            user: 'leonardo',
-            password: 'leonardo',
-            database: 'wester_tutom_001'
-        });
         
-        conn.query('select * from produtos', function(err, result){
-            res.send(result);
+        conn.query('select * from livros', function(err, result){
+            res.render('produtos/lista', {lista: result});
         });
-        conn.end();
+        //conn.end();
         //res.send('olá mundo!');
         ///lista Ã© o arquivo EJS (a extensÃ£o foi omitida, pois o mÃ³dulo EJS identifica a extensÃ£o)
         //res.render('produtos/lista');
+    });
+    
+    app.get('/produtos/:id', function(req, res){
+        conn.query('select * from livros where id='+req.params.id, function(err, result){
+            res.send(result);
+        });
+        
     });
 };
