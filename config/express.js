bc4b0d78dd0dbb407a15e527500da4849a6a3553
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-08-05 16:28:59 -0300

    Add comentários

--- config/express.js
+++ config/express.js
@@ -4,25 +4,25 @@ var load = require('express-load');
 var app = express();
 var bodyParser = require('body-parser');
 
-//insere um middleware
+//use() insere um middleware
 app.use(bodyParser.urlencoded({extended: true}));
 
 //set() seta as variáveis para o ambiente. EJS tem uma variável chamada view engine
 //ejs é o nome da engine que foi instalada para o projeto.
 app.set('view engine', 'ejs');
 //o caminho passado aqui é apartir de quem chamou engine, no caso index.js
-//config da pasta onde ficarão as rotas
+//config da pasta onde ficarão as views
 app.set('views', './app/views');
 
 app.listen(port, function(req, res){
     console.log('Servidor rodando na porta ' + port);
 });
 module.exports = function(){
-    //carrega as rotas automaticamente
+    //carrega as módulos automaticamente
     //cwd é o ponto de onde o express-load começa a procurar as rotas
-	load('route', {cwd: 'app'})
-		.then('infra')
-		.into(app);
+	load('route', {cwd: 'app'})//carrega os módulos relacionados as rotas
+		.then('infra')//correga os módulos relacionados ao DAO
+		.into(app);//declara a pasta onde o load() deve iniciar as buscas por módulos
 
     return app;
 };
